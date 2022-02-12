d684dd8648f512407e7549224bfb5283ffb13859
nurmuhamadsoleh <nursoleh33@gmail.com>
2022-02-13 01:04:53 +0700

    first commit

---
+++ src/components/template-surat/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "reactstrap";
+const Templatesurat = () =>{
+    return(
+        <Container>
+            <h1>Test</h1>
+        </Container>
+    )
+}
+export default Templatesurat;
---
+++ src/pages/lapor-surat-page/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { KopSurat } from '../../components'
+import Templatesurat from "../../components";
+
+const LaporSurat = () =>{
+    return(
+        <div>
+            <KopSurat/>
+        </div>
+    )
+}
+export default LaporSurat;
